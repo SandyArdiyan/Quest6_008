d5b864172481979809003448000e906272b5fb0c
SandyArdiyan <sanardiyan881@gmail.com>
2025-11-21 16:27:37 +0700

    Agar bisa di run

--- app/src/main/java/com/example/mvvmprak8/MainActivity.kt
+++ app/src/main/java/com/example/mvvmprak8/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvmprak8.ui.theme.MVVMPrak8Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +17,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             MVVMPrak8Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MVVMPrak8Theme {
-        Greeting("Android")
     }
 }
